912707ce12fcb214fdbac15b14a883c7ed9816ee
張旭 <zx1986@gmail.com>
2015-05-26 15:33:06 +0800

    try to use Homebrew Cask

--- xMac.sh
+++ xMac.sh
@@ -7,3 +7,13 @@ ruby='rbenv ruby-build'
 toys='archey cowsay figlet'
 
 brew install ${system} ${shell} ${coding} ${ruby} ${toys}
+
+brew install caskroom/cask/brew-cask
+brew cask install atom
+brew cask install skype
+brew cask install iterm2
+brew cask install firefox
+brew cask install google-chrome
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install boot2docker
