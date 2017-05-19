d93d16cec4a955d8f5ed6c3739831359823b6507
張旭 <zx1986@gmail.com>
2017-05-19 08:46:31 +0800

    調整裝機套件，補上 antigen, fzf

--- xUbuntu.sh
+++ xUbuntu.sh
@@ -2,7 +2,7 @@
 
 apt='sudo apt-get install'
 
-coding='vim vim-doc ack-grep git git-doc git-flow tig'
+coding='vim vim-doc ack-grep git git-doc git-flow-avh tig'
 network='nmap iftop nethogs netcat ngrep iptraf curl'
 system='openssh-server htop dstat sysv-rc-conf mosh bum guake'
 server='nginx-extras apache2 haproxy hatop squid3'
@@ -10,5 +10,6 @@ web='tsung siege'
 graphic='inkscape gimp'
 video='pitivi kdenlive'
 game='slashem nethack-console'
+shell='zsh antigen'
 
-${apt} ${network} ${system} ${coding}
+"${apt}" "${network}" "${system}" "${coding}" "${shell}"
