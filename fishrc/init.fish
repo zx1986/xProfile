ca0ddaacf0971f3427f3c0a10f57f83f12aec13e
張旭 <zx1986@gmail.com>
2018-11-23 12:22:43 +0800

    feat: 調整 Fish shell 的 PATH
    https://github.com/fish-shell/fish-shell/issues/527

--- fishrc/init.fish
+++ fishrc/init.fish
@@ -10,6 +10,16 @@ set -xg PYTHONDONTWRITEBYTECODE 1
 # Homebrew read Github
 set -xg HOMEBREW_GITHUB_API_TOKEN super_secret_token
 
+# PATH
+set -gx PATH \
+        $PATH \
+        $GOPATH/bin \
+        /usr/local/opt/nss/bin \
+        /usr/local/opt/ncurses/bin \
+        /usr/local/opt/gettext/bin \
+        /usr/local/opt/mysql-client/bin \
+        /usr/local/opt/go/libexec/bin
+
 # gitignore.io
 function gi
 	curl -L -s https://www.gitignore.io/api/$argv
