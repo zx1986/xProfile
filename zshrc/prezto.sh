44cbc2cea01aa2499ed6a0bfde9fa5cc8efc5a1e
張旭 <zx1986@gmail.com>
2024-07-24 11:22:36 +0800

    feat: setup prezto

---
+++ zshrc/prezto.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+git clone https://github.com/agkozak/zsh-z.git ~/.zprezto-contrib/zsh-z
+
+cat <<EOF >> ${ZDOTDIR:-$HOME}/.zshrc
+autoload -U +X bashcompinit && bashcompinit
+autoload -U +X compinit && compinit
+
+source $(PWD)/zshrc/zsh_pre_setup
+source $(PWD)/zshrc/omz-git.zsh
+source $(PWD)/zshrc/omz-git.plugin.zsh
+source $(PWD)/zshrc/zsh_post_setup
+EOF
+
+# https://github.com/sorin-ionescu/prezto
+# https://wikimatze.de/better-zsh-with-prezto/
